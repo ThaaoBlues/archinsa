7d5867f55f47eaee5f8c45ae6f2d3bbc4f7d0bc0
thaaoblues <thaaoblues81@gmail.com>
2024-10-17 18:53:42 +0200

    aie aie aie l'inversement des arguments x'(

--- utils/inputs.php
+++ utils/inputs.php
@@ -9,7 +9,7 @@ function assainir_et_valider_mel($og_mel): string {
 
     // Vérifie si l'adresse e-mail est valide
     $reg_pattern = "/^[a-zA-Z0-9._%+-]+@insa-toulouse\.fr$/";
-    if (filter_var($mel, FILTER_VALIDATE_EMAIL) && preg_match($mel,$reg_pattern)) {
+    if (filter_var($mel, FILTER_VALIDATE_EMAIL) && preg_match($reg_pattern,$mel)) {
         return $mel; // Si valide, renvoie l'adresse e-mail assainie
     } else {
         return "[ERREUR_MEL_MALSAINT]"; // Sinon, renvoie un message d'erreur
